fbf5324a5c36b3cb28228ef6b6f63413c4f339d5
sandip-gadekar <57319706+sandip-gadekar@users.noreply.github.com>
2022-09-05 14:53:47 +0530

    Updates

---
+++ precache-manifest.202c616e5a197a932c2d80cfb0884696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "860b68424664cdd34cea",
+    "url": "/Textutiles/static/css/main.b0be18c2.chunk.css"
+  },
+  {
+    "revision": "860b68424664cdd34cea",
+    "url": "/Textutiles/static/js/main.860b6842.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/Textutiles/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "e3ca8c8554beae76c01b",
+    "url": "/Textutiles/static/js/2.e3ca8c85.chunk.js"
+  },
+  {
+    "revision": "936333d0965aee0ad4ff",
+    "url": "/Textutiles/static/js/runtime~main.936333d0.js"
+  },
+  {
+    "revision": "0a1d46d02692d7b4f86fc09e923740ec",
+    "url": "/Textutiles/index.html"
+  }
+];
